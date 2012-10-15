712b1913f30e87901876a7986d03bfd9e4ec6260
Andrew Dolgov <fox@fakecake.org>
2012-10-15 11:59:51 +0400

    implement directory chooser

--- src/org/fox/ttcomics/PreferencesActivity.java
+++ src/org/fox/ttcomics/PreferencesActivity.java
@@ -1,7 +1,9 @@
 package org.fox.ttcomics;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Environment;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
@@ -13,15 +15,45 @@ public class PreferencesActivity extends PreferenceActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		SharedPreferences prefs = PreferenceManager
+		final SharedPreferences prefs = PreferenceManager
 				.getDefaultSharedPreferences(getApplicationContext());
 		
 		addPreferencesFromResource(R.xml.preferences);
 		
 		if (CommonActivity.isCompatMode()) {
-			Preference pref = findPreference("dim_status_bar");
-			PreferenceCategory cat = (PreferenceCategory) findPreference("prefs_reading");
-			cat.removePreference(pref);			
+			Preference dimPref = findPreference("dim_status_bar");
+			PreferenceCategory readingCat = (PreferenceCategory) findPreference("prefs_reading");
+			readingCat.removePreference(dimPref);			
+		}
+		
+		Preference dirPref = (Preference) findPreference("comics_directory");
+		dirPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+				Intent intent = new Intent(PreferencesActivity.this, DirectoryPicker.class);
+				
+				//intent.putExtra(DirectoryPicker.START_DIR, prefs.getString("comics_directory", 
+				//		Environment.getExternalStorageDirectory().getAbsolutePath()));
+				
+				startActivityForResult(intent, DirectoryPicker.PICK_DIRECTORY);				
+				return true;
+             }
+         });
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(requestCode == DirectoryPicker.PICK_DIRECTORY && resultCode == RESULT_OK) {
+			Bundle extras = data.getExtras();
+			String path = (String) extras.get(DirectoryPicker.CHOSEN_DIRECTORY);
+			
+			SharedPreferences prefs = PreferenceManager
+					.getDefaultSharedPreferences(getApplicationContext());
+
+			SharedPreferences.Editor editor = prefs.edit();
+			editor.putString("comics_directory", path);	    	
+	    	editor.commit();
+			
 		}
 	}
 }
