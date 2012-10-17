6034cd4579c41c27185cc4a9502b167052d0adfd
Andrew Dolgov <fox@fakecake.org>
2012-10-17 12:15:16 +0400

    experimental switch to webview

--- src/org/fox/ttcomics/ComicFragment.java
+++ src/org/fox/ttcomics/ComicFragment.java
@@ -1,20 +1,28 @@
 package org.fox.ttcomics;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -24,6 +32,7 @@ public class ComicFragment extends Fragment {
 	private SharedPreferences m_prefs;
 	private int m_page;
 	private CommonActivity m_activity;
+	private File m_imageFile;
 	
 	public ComicFragment() {
 		super();
@@ -34,7 +43,7 @@ public class ComicFragment extends Fragment {
 		m_page = page;
 	}
 	
-	public Bitmap loadImage(ComicArchive archive, int page) {
+	/* public Bitmap loadImage(ComicArchive archive, int page) {
 		CommonActivity activity = (CommonActivity) getActivity();
 		
 		try {			
@@ -59,14 +68,15 @@ public class ComicFragment extends Fragment {
 		}
 		
 		return null;
-	}
+	} */
 	
+	@SuppressLint("NewApi")
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {    	
 		
 		View view = inflater.inflate(R.layout.fragment_comic, container, false);
 		
-		TouchImageView image = (TouchImageView) view.findViewById(R.id.comic_image);
+		final WebView web = (WebView) view.findViewById(R.id.comic);
 		
 		if (savedInstanceState != null) {
 			m_page = savedInstanceState.getInt("page");
@@ -76,27 +86,89 @@ public class ComicFragment extends Fragment {
 		
 		if (pager != null) {
 			if (CommonActivity.isCompatMode() && m_prefs.getBoolean("use_dark_theme", false)) {
-				image.setBackgroundColor(0xff000000);
+				web.setBackgroundColor(0xff000000);
 			}
 			
+			WebSettings ws = web.getSettings();
+			ws.setSupportZoom(true);
+			ws.setBuiltInZoomControls(true);
+			ws.setCacheMode(WebSettings.LOAD_NO_CACHE);
+			//ws.setUseWideViewPort(true);
+			ws.setLoadWithOverviewMode(true);
 			
-			image.setImageBitmap(loadImage(pager.getArchive(), m_page));
-			image.setMaxZoom(4f);
-			image.setOnScaleChangedListener(new TouchImageView.OnScaleChangedListener() {
+		    // prevent flicker in ics
+		    if (android.os.Build.VERSION.SDK_INT >= 11) {
+		    	web.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+		    }
+    	    
+		    AsyncTask<InputStream, Void, File> loadTask = new AsyncTask<InputStream, Void, File>() {
 				@Override
-				public void onScaleChanged(float scale) {
-					ViewPager pager = (ViewPager) getActivity().findViewById(R.id.comics_pager);
+				protected File doInBackground(InputStream... params) {
+					try {
+				    	File imageFile = File.createTempFile("tcrview", ".jpg", getActivity().getExternalCacheDir());
+				    	
+						InputStream in = params[0];
+						FileOutputStream out = new FileOutputStream(imageFile);
+						
+						int c;
+						while ((c = in.read()) != -1) {
+							out.write(c);
+						}
+						
+						out.close();
+						in.close();
+						
+						return imageFile;
+						
+					} catch (IOException e) {				
+						e.printStackTrace();
+					} catch (OutOfMemoryError e ) {
+						e.printStackTrace();
+					}
 					
-					if (pager != null) {
-						pager.setPagingEnabled(scale - 1.0f < 0.01);
+					return null;
+				}
+				
+				@Override
+				protected void onPostExecute(File result) {
+					if (getActivity() != null && isAdded()) {
+						if (result != null) {
+							String url = "file://" + result.getAbsolutePath();
+							
+							String content = "<html>" +
+									"<head>" +
+									"<meta content=\"text/html; charset=utf-8\" http-equiv=\"content-type\">" +
+									"<style type=\"text/css\">" +
+									"body { padding : 0px; margin : 0px; background : transparent; }" +
+									"</style>" +
+									"</head>" +
+									"<body><table width='100%' height='100%'>" +
+									"<tr><td><img width='100%' src=\""+ url  +"\"></td></tr>" +
+									"</table></body></html>";
+						    
+							Log.d(TAG, content);
+							
+							web.loadDataWithBaseURL(null, content, "text/html", "utf-8", null);
+							
+							m_imageFile = result;
+						} else {
+							((CommonActivity) getActivity()).toast(R.string.error_loading_image);
+						}
 					}
 				}
-			});
-
-			image.setCustomOnTouchListener(new View.OnTouchListener() {
+		    	
+		    };
+		    
+		    try {
+				loadTask.execute(pager.getArchive().getItem(m_page));
+			} catch (IOException e) {
+				e.printStackTrace();
+			} 
+		    
+			web.setOnTouchListener(new View.OnTouchListener() {
 				int m_x;
 				int m_y;
-
+	
 				@Override
 				public boolean onTouch(View view, MotionEvent event) {
 					switch (event.getAction()) {
@@ -123,12 +195,12 @@ public class ComicFragment extends Fragment {
 				}
 			});
 			
-		}
+			TextView page = (TextView) view.findViewById(R.id.comic_page);
+			
+			if (page != null) {
+				page.setText(String.valueOf(m_page+1));
+			}
 		
-		TextView page = (TextView) view.findViewById(R.id.comic_page);
-		
-		if (page != null) {
-			page.setText(String.valueOf(m_page+1));
 		}
 
 		return view;
@@ -141,6 +213,15 @@ public class ComicFragment extends Fragment {
 
 	private void onRightSideTapped() {
 		m_activity.selectNextComic();
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		
+		if (m_imageFile != null) {
+			m_imageFile.delete();
+		}
 	}
 
 	@Override
