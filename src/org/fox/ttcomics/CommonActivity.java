677699414dbdda250fcdf71a5e51b60ef370cbaa
Andrew Dolgov <fox@fakecake.org>
2012-10-29 19:30:35 +0400

    fix sha1 crashing on null string

--- src/org/fox/ttcomics/CommonActivity.java
+++ src/org/fox/ttcomics/CommonActivity.java
@@ -367,19 +367,21 @@ public class CommonActivity extends FragmentActivity {
 	}
 
 	protected static String sha1(String s) {
-		try {
-			MessageDigest digest = java.security.MessageDigest.getInstance("SHA1");
-	        digest.update(s.getBytes());
-	        byte messageDigest[] = digest.digest();
-	        
-	        StringBuffer hexString = new StringBuffer();
-	        for (int i=0; i<messageDigest.length; i++)
-	            hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
-	        
-	        return hexString.toString();
-	        
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
+		if (s != null) {
+			try {
+				MessageDigest digest = java.security.MessageDigest.getInstance("SHA1");
+		        digest.update(s.getBytes());
+		        byte messageDigest[] = digest.digest();
+		        
+		        StringBuffer hexString = new StringBuffer();
+		        for (int i=0; i<messageDigest.length; i++)
+		            hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+		        
+		        return hexString.toString();
+		        
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
 		}
 		
 		return null;
